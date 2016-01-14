ac7da7c933a2828b193b51b787c896f2af38403c
Andrew Price <adprice@Andrews-MacBook.local>
2016-01-14 01:18:40 -0500

    Changed test_client.py to actually invoke client code to connect to server. This helps validate that part in addition to server changes.

--- server/test_client.py
+++ server/test_client.py
@@ -1,68 +1,32 @@
 #!/usr/bin/python
-import socket, ssl
-import json
 import sys
-
-def send_vera_message(s, data):
-    # Encode the message, send to Vera, and wait for response
-    msg = json.dumps(data)
-    print ('sending msg: ' + msg)
-    s.sendall(msg)
-
-    # Wait for a response
-    resp = s.read()
-    print ('resp: ' + resp)
-        
-    # Decode the received message
-    return json.loads(resp)
+import os
 
 def main():
-    # Define the port/hostname for the server that we will connect to
-    PORT = 3000
-    HOST = '0.0.0.0' # FIXME
-    
-    # Define paths to our security assets
-    CA_PATH = './base_rootCA.pem'
-    CLIENT_CERT_PATH = './lambda.crt'
-    CLIENT_KEY_PATH = './lambda.key'
-    
-    print 'Configuring security parameters.'
-    
-    # Create the SSL context to authenticate server
-    context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
-    context.load_verify_locations(CA_PATH)
-    context.load_cert_chain(certfile=CLIENT_CERT_PATH, keyfile=CLIENT_KEY_PATH)
-    context.verify_mode = ssl.CERT_REQUIRED
-    
-    # Create the socket and wrap it in our context to secure
-    # By specifying server_hostname we require the server's certificate to match the
-    # hostname we provide
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    secure_s = context.wrap_socket(s, server_hostname='hostname.dynamic-dns.net') # FIXME
-    print 'connect to ' + HOST + ':' + str(PORT)
-    secure_s.connect((HOST, PORT))
-    
-    # Send a test message
-    device = '18'
-    action = 'on'
-    if action == 'on':
-        value = 1
-    elif action == 'off':
-        value = 0
-    else:
-        value = 0
-    data = { 'id':int(device), 
-                'action': {'type': 'set', 'attribute': {'power':value} },
-                'close_connection':True }
-    resp = send_vera_message(secure_s, data)
-    if resp['status'] == 0:
-        print 'OK.'
-    else:
-        print 'Error. ' + resp['err_str']
+    # To run the client code we have to change to that directory since a bunch of
+    # files in there are pathed relative to that directory. We also have to add that
+    # path to the module import path.
+    os.chdir('../lambda')
+    sys.path.append('./')
 
-    # Close the socket and exit
-    secure_s.close()
-    print 'closed connection'
+    # Actually use the client code to test sending server message
+    import client
+
+    # Try connecting to the Vera server
+    (socket, msg) = client.open_connection_to_vera()
+    if socket == None:
+        print 'Error connecting to Vera: ' + msg
+        sys.exit()
+
+    # TEST: Run scene 1
+    data = { 'id':1, 'action': {'type': 'run' }, 'close_connection':True }
+    resp = client.send_vera_message(socket, data)
+    print resp
+
+    # TODO - add more test cases
+
+    # Close the connection
+    client.close_connection_to_vera(socket)
     sys.exit()
         
 if __name__ == '__main__':
