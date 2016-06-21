b9013acbe3a9574f85d2119874cd3a9444a12110
Andrew Price <andprice@lab126.com>
2016-06-20 17:01:27 -0700

    Some more robust testing of SSL contexts and fixed the "no shared cipher" error for ssl without certs. I'm still not totally sure why it happened, but manually creating the context and specifying protocols/certs seems to make it work.

--- security/security_test.py
+++ security/security_test.py
@@ -10,15 +10,90 @@ import sys
 import pkgutil
 
 # Global variables
-#BASE_PATH='./user'
 BASE_PATH='./sample'
 
 CA_PATH = BASE_PATH + '/rootCA.pem'
 PORT = 3000
 HOST = 'localhost'
 
+# Function that configures and starts the server
+def start_server(security, lock):
+
+    # Location of server cert/key
+    SERVER_CERT_PATH = BASE_PATH + '/server.crt'
+    SERVER_KEY_PATH = BASE_PATH + '/server.key'
+
+    print ('server: configuring security profile as "' + security + '"')
+    
+    # Open up the port and listen for connections
+    # Create the socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    # Bind the socket to the port
+    print 'server: started on ' + socket.gethostname() + ':' + str(PORT)
+    print
+    # Do some error checking as binds can fail if the port is being used by
+    # someone else
+    try:
+        s.bind(('', PORT))
+    except socket.error as msg:
+        print 'server: socket bind() failed!'
+        print 'server: (err ' + str(msg[0]) + '): ' + msg[1]
+        sys.exit()
+
+    # start listening (with max 3 connections queued)
+    s.listen(3)
+
+    # Setup the SSL context based on assets provided in the config file
+    if security == 'none':
+        # No need to create an SSL context
+        pass
+    elif security == 'ssl':
+        context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+        context.set_ciphers('HIGH')
+        context.check_hostname = False
+        context.verify_mode = ssl.CERT_NONE
+    elif security == 'ssl_mutual_auth':
+        context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+        context.load_verify_locations(CA_PATH)
+        context.verify_mode = ssl.CERT_REQUIRED
+        context.load_cert_chain(certfile=SERVER_CERT_PATH, keyfile=SERVER_KEY_PATH)
+
+    # Wait here for a client to connect
+    # accept() will block until a client has tried to connect
+    (new_s, addr) = s.accept()
+    print 'server: connection from ' + addr[0] + ':' + str(addr[1])
+        
+    # Wrap the socket in our SSL context to protect communications
+    if security == 'none':
+        secure_s = new_s
+    else:
+        secure_s = context.wrap_socket(new_s, server_side=True)
+
+    # Print out the peer (client) certificate
+    if security == 'ssl_mutual_auth':
+        lock.acquire()
+        print ('CLIENT CERTIFICATE (as seen by server):')
+        cert = secure_s.getpeercert()
+        pprint.pprint(cert)
+        print
+        lock.release()
+
+    # Get client data and send the response
+    data = secure_s.recv(1024)
+    lock.acquire()
+    print ('server: msg from client "' + data + '"')
+    lock.release()
+    secure_s.sendall(b'OK')
+    lock.acquire()
+    print ('server: sent response')
+    lock.release()
+
+    # Return the socket handle so we can cleanup before starting the next server
+    return secure_s
+
 # This is the main function for the client thread
-def client_thread(lock):
+def client_thread(security, lock):
     # Wait for the server to start listening then send a message
     time.sleep(1)
 
@@ -26,58 +101,78 @@ def client_thread(lock):
     CLIENT_CERT_PATH = BASE_PATH + '/client.crt'
     CLIENT_KEY_PATH = BASE_PATH + '/client.key'
     
-    print ('client: configuring security parameters.')
+    print ('client: configuring security profile as "' + security + '"')
     
-    # Create the SSL context to authenticate server
-    context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
-    context.load_verify_locations(CA_PATH)
-    context.load_cert_chain(certfile=CLIENT_CERT_PATH, keyfile=CLIENT_KEY_PATH)
-    context.verify_mode = ssl.CERT_REQUIRED
-    
-    # Create the socket and wrap it in our context to secure
-    # By specifying server_hostname we require the server's certificate to match the
-    # hostname we provide
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    secure_s = context.wrap_socket(s, server_hostname=HOST)
-    print ('client: connect to ' + HOST + ':' + str(PORT))
-    
+    # Try a regular connection (INSECURE)
+    sock = None
+    if security == 'none':
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print ('client: connect to ' + HOST + ':' + str(PORT) + ' (INSECURE)')
+    elif security == 'ssl':
+        # For this example we show TLSv1. You could also try SSLv2 or SSLv3 but
+        # both of these protocols are known to be insecure and more recent versions
+        # of OpenSSL compiled with NO_SSLv2 and NO_SSLv3 flags won't allow this option.
+        context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+        # The string passed here sets the OpenSSL cipher we're willing to use.
+        # See: https://www.openssl.org/docs/manmaster/apps/ciphers.html#CIPHER-LIST-FORMAT
+        # HIGH is basically >128 bit ciphers.
+        context.set_ciphers('HIGH')
+        context.check_hostname = False
+        context.verify_mode = ssl.CERT_NONE
+    elif security == 'ssl_mutual_auth':
+        context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+        context.load_verify_locations(CA_PATH)
+        context.verify_mode = ssl.CERT_REQUIRED
+        context.load_cert_chain(certfile=CLIENT_CERT_PATH, keyfile=CLIENT_KEY_PATH)
+
+    if security != 'none':
+        # Create the socket and wrap it in our context to secure
+        # By specifying server_hostname we require the server's certificate to match the
+        # hostname we provide
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock = context.wrap_socket(s, server_hostname=HOST)
+        print ('client: connect to ' + HOST + ':' + str(PORT) + ' (SSL/TLS)')
+
+    # Try to make the connection
     try:
-        secure_s.connect((HOST, PORT))
+        sock.connect((HOST, PORT))
     except socket.error as msg:
-        print ('client: socket error: ' + msg[1])
+        print ('client: socket error (' + str(msg[0]) + '): ' + msg[1])
         return
 
     # Print out some info about our connection
-    lock.acquire()
-    print ('SERVER CERTIFICATE (as seen by client):')
-    cert = secure_s.getpeercert()
-    pprint.pprint(cert)
-    print
-    print ('CONNECTION DETAILS:')
-    cipher = secure_s.cipher()
-    print ('cipher: ' + cipher[0] + ', protocol: ' + cipher[1] + ', bits: ' + str(cipher[2]))
-    if (secure_s.compression() is not None):
-        print ('compression: ' + secure_s.compression())
-    else:
-        print ('compression: None')
-    print ('protocol version: ' +secure_s.version())
-    print
-    lock.release()
+    if security != 'none':
+        lock.acquire()
+        if security == 'ssl_mutual_auth':
+            print ('SERVER CERTIFICATE (as seen by client):')
+            cert = sock.getpeercert()
+            pprint.pprint(cert)
+            print
+        print ('CONNECTION DETAILS:')
+        cipher = sock.cipher()
+        print ('cipher: ' + cipher[0] + ', protocol: ' + cipher[1] + ', bits: ' + str(cipher[2]))
+        if (sock.compression() is not None):
+            print ('compression: ' + sock.compression())
+        else:
+            print ('compression: None')
+        print ('protocol version: ' + sock.version())
+        print
+        lock.release()
 
     # Send the data
-    secure_s.sendall(b'This is a test message.')
+    sock.sendall(b'This is a test message.')
     lock.acquire()
     print ('client: sent message to server')
     lock.release()
 
     # Wait for a response
-    data = secure_s.read()
+    data = sock.recv(1024)
     lock.acquire()
     print ('client: msg from server "' + data + '"')
     lock.release()
 
     # Close the socket
-    secure_s.close()
+    sock.close()
 
 def main():
     # Check for the right Python version
@@ -139,75 +234,54 @@ def main():
     print
     
     #
-    # Setup a mutually authenticated client/server running on localhost
+    # Setup client/server running on localhost with various security parameters
     #
 
     # Check OpenSSL version
+    print ('OpenSSL version is:')
     print (ssl.OPENSSL_VERSION)
 
-    # Kick off a client thread to send a message to the server
+    # Create a lock for synchronization between threads
     lock = threading.Lock()
-    t = threading.Thread(target=client_thread, args=(lock,))
+
+    # 1) none - just do regulat connection (INSECURE)
+    # Kick off a client thread to send a message to the server
+    # This is done first because start_server() block waiting for a client
+    # to connect.
+    print
+    print ('--------------------------------')
+    print ('    TEST 1 - Standard socket')
+    print ('--------------------------------')
+    security = 'none'
+    t = threading.Thread(target=client_thread, args=(security, lock,))
     t.start()
-
-    # Location of server cert/key
-    SERVER_CERT_PATH = BASE_PATH + '/server.crt'
-    SERVER_KEY_PATH = BASE_PATH + '/server.key'
-    
-    print ('server: configuring security for SSL mutual authentication.')
-    
-    # Create the SSL context to authenticate client
-    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-    context.load_verify_locations(CA_PATH)
-    context.load_cert_chain(certfile=SERVER_CERT_PATH, keyfile=SERVER_KEY_PATH)
-    context.verify_mode = ssl.CERT_REQUIRED
-    
-    # Create the socket (For SSL we must use SOCK_STREAM)
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-    # Bind the socket to the port
-    print ('server: starting on ' + socket.gethostname() + ':' + str(PORT))
-    print
-    # Do some error checking as binds can fail if the port is being used by
-    # someone else
-    try:
-        s.bind(('', PORT))
-    except socket.error as msg:
-        print ('server: socket bind() failed!')
-        print ('server: (err ' + str(msg[0]) + '): ' + msg[1])
-        sys.exit()
-        
-    # start listening (with max 3 connections queued)
-    s.listen(3)
-
-    # accept the connection when it is received
-    (new_s, addr) = s.accept()
-    print ('server: connection from ' + addr[0] + ':' + str(addr[1]))
-        
-    # Wrap the socket in our SSL context to protect communications 
-    secure_s = context.wrap_socket(new_s, server_side=True)
-
-    # Print out the peer (client) certificate
-    lock.acquire()
-    print ('CLIENT CERTIFICATE (as seen by server):')
-    cert = secure_s.getpeercert()
-    pprint.pprint(cert)
-    print
-    lock.release()
-
-    # Get client data and send the response
-    data = secure_s.read()
-    lock.acquire()
-    print ('server: msg from client "' + data + '"')
-    lock.release()
-    secure_s.sendall(b'OK')
-    lock.acquire()
-    print ('server: sent response')
-    lock.release()
-
-    # Wait for the client thread to finish
+    s = start_server(security, lock)
     t.join()
-    secure_s.close()
+    s.close()
+
+    # 2) just the section - use ssl/tls but with no auth
+    print
+    print ('--------------------------------')
+    print ('    TEST 2 - SSL/TLS No auth')
+    print ('--------------------------------')
+    security = 'ssl'
+    t = threading.Thread(target=client_thread, args=(security, lock,))
+    t.start()
+    s = start_server(security, lock)
+    t.join()
+    s.close()
+
+    # 3) root_ca plus client cert/key- give out certificate to server
+    print
+    print ('--------------------------------')
+    print ('  TEST 3 - SSL/TLS Mutual auth')
+    print ('--------------------------------')
+    security = 'ssl_mutual_auth'
+    t = threading.Thread(target=client_thread, args=(security, lock,))
+    t.start()
+    s = start_server(security, lock)
+    t.join()
+    s.close()
 
     # Exit
     sys.exit()
